4159de6a741a9191ded465c852059952f9ed6b54
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-12-03 03:37:56 +0700

    :art: Refactor ChartComponent to use Chart.js

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -1,23 +1,46 @@
-import Chart from 'chart.js/auto';
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import { Radar } from 'react-chartjs-2';
 
-export default function ChartComponent({data}: any) {
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
 
-    return(
-      <Radar
-        updateMode='resize'
-        datasetIdKey='id'
-        data={{
-          labels: data?.chartData?.labels,
-          datasets: [
-            {
-              label: data?.chartData?.datasets?.label,
-              data: data?.chartData?.datasets?.data,
-              // borderColor: '#957CB6',
-              // backgroundColor: '#957CB6'
-            },
-          ],
-        }}
-      />
-    )
+export default function ChartComponent({ data }: any) {
+
+  return (
+    <Radar
+    updateMode='resize'
+    datasetIdKey='id'
+    
+    data={{
+      labels: data?.chartData?.labels,
+      datasets: [
+        {
+          label: data?.chartData?.datasets?.label,
+          data: data?.chartData?.datasets?.data,
+          // borderColor: '#957CB6',
+          // backgroundColor: '#957CB6',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 2)',
+          borderWidth: 1,
+          
+        },
+      ],
+    }}
+    />
+
+  )
 }
