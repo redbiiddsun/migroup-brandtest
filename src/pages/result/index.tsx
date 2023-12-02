8f3e4fce75d41cdcff66881558ef212917092b7f
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-12-03 05:05:05 +0700

    :sparkles: update result page components and update data handling

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,39 +1,42 @@
 import Image from "next/image";
-import type { InferGetServerSidePropsType, GetServerSideProps, GetStaticProps } from 'next'
+import type { GetServerSideProps, } from 'next'
+
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-// import ChartComponent from "@/component/chartComponent";
+import summaryData from "@/utils/summarydata";
 
-// Component
-// import Iconic from "@/component/Iconic";
-// import Descriptions from "@/component/description";
-// import MarketingStrategy from "@/component/marketingStrategy";
-// import CommunicationStrategy from "@/component/CommunicationStrategy";
-// import MiGroupRole from "@/component/MIGroup";
+// Components
+import Header from "@/components/Header";
+import ChartComponent from "@/components/ChartComponent";
+import Descriptions from "@/components/description";
+import MarketingStrategy from "@/components/MarketingStrategy";
+import CommunicationStrategy from "@/components/CommunicationStrategy";
+import MiGroupRole from "@/components/MIGroup";
 
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 
-    return {
-      props: {
-        ...(await serverSideTranslations(context.locale ?? 'en',)),
-      },
+    let somthing: result = {
+        BrandPerception: Number(context.query.BrandPerception),
+        BrandValue: Number(context.query.BrandValue),
+        MarketEngagement: Number(context.query.MarketEngagement),
+        CustomerRelationship: Number(context.query.CustomerRelationship),
+        ProductExcellence: Number(context.query.ProductExcellence),
     };
-  };
+
+    const data = summaryData(somthing, context.locale ?? 'en')
+
+    return {
+        props: {
+            ...(await serverSideTranslations(context.locale ?? 'en')),
+            data: data
+        },
+    };
+};
 
 
-// export default function Page(_props: InferGetServerSidePropsType<typeof getServerSideProps>){
-    
-//     return (
-//         <div>
-//             <p>{t('result.name')}</p>
-//         </div>
-//     )
-// }
-
-
-export default function resultPage({ message, data }: any) {
+export default function Page({ data }: { data: summaryData }) {
 
     const { t } = useTranslation('common')
 
@@ -52,34 +55,35 @@ export default function resultPage({ message, data }: any) {
                         width={10000}
                         height={10000}
                         alt="background"
-                        src={message?.animalDetail?.image}
+                        src={data?.animalDetail?.image}
                         className="absolute top-0 left-0 z-10 lg:rounded-2xl md:rounded-2xl"
                     />
                     <div className="sticky w-full z-30">
-                        {/* <Iconic img={data?.animalDetail.logo} data={data}/> */}
-                        <div className="text-base pt-3 px-3 md:px-28 lg:px-52 text-center">
-                            {message?.animalDetail?.shortdescription}
+                        <Header title={t('title')} img={data.animalDetail.logo} data={data} />
+                        <div className={`text-base pt-3 px-7 md:px-28 lg:px-52 text-center`} >
+                            {data?.animalDetail?.shortdescription}
                         </div>
                         <div className="text-center">
                             <div className="text-xl lg:text-24 pt-9">
                                 {t('brandchartTitle')}
                             </div>
-                            <div className="text-2xl text-indigo">
-                                {message?.animalDetail?.brandChart}
+                            <div className={`text-2xl text-indigo text-[${data?.animalDetail?.color}]`}>
+                                {data?.animalDetail?.brandChart}
                             </div>
                         </div>
                         <div className="mx-0  lg:px-44">
-                            {/* <ChartComponent data={data} className="px-4" /> */}
+                            <ChartComponent data={data} className="px-4" />
                         </div>
                     </div>
                     <div className="px-9 pb-9">
-                        {/* <Descriptions data={data} topic={...uiEN}/>
-                        <MarketingStrategy data={data} topic={...uiEN} />
-                        <CommunicationStrategy data={data} topic={...uiEN} />
-                        <MiGroupRole data={data} topic={...uiEN}/> */}
+                        <Descriptions data={data} t={t} />
+                        <MarketingStrategy data={data} t={t} />
+                        <CommunicationStrategy data={data} t={t} />
+                        <MiGroupRole data={data} t={t} />
                     </div>
                 </div>
             </div>
         </>
     );
 }
+
