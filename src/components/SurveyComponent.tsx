e2cc95555b9f4898470c4cd8ae4691b50d783676
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-12-02 19:47:46 +0700

    :sparkles: add a survey component

--- src/components/SurveyComponent.tsx
+++ src/components/SurveyComponent.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
-import { totalPoint } from '../../utills/result'
+import { totalPoint } from '@/utils/result'
 import { Theme } from "@/theme/theme"
 
 
-export default function SurveyComponent({model}) {
+export default function SurveyComponent({model}: {model: Question} ) {
 
     const survey = new Model(model);
     survey.applyTheme(Theme);
     
     survey.onComplete.add((sender, options) => {
-        // const data = totalPoint(sender.data, model.type)
+        const data = totalPoint(sender.data, model.type)
+
+        console.log(data)
 
         // if(model.locale == 'th'){
         //     document.location.replace(`../result/th?BrandPerception=${data.BrandPerception}&BrandValue=${data.BrandValue}&MarketEngagement=${data.MarketEngagement}&CustomerRelationship=${data.CustomerRelationship}&ProductExcellence=${data.ProductExcellence}`)
