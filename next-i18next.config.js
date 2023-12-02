7f34fae7b01222a5c978b73456d52caa7939e549
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-12-03 05:33:37 +0700

    :ambulance: fix issues deploying vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -11,9 +11,9 @@ module.exports = {
         localeDetection: false,
     },
      /** To avoid issues when deploying to some paas (vercel...) */
-    // localePath:
-    //     typeof window === "undefined"
-    //         ? require("path").resolve("./public/locales/")
-    //         : "/locales",
+    localePath:
+        typeof window === "undefined"
+            ? require("path").resolve("./public/locales/")
+            : "/locales",
 
 };
