7054aa36be3f25d85b6ba20dc0289a9bc95471a2
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-12-04 18:38:28 +0700

    :sparkles: add a graph option

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -20,25 +20,42 @@ ChartJS.register(
 
 export default function ChartComponent({ data }: any) {
 
+  const options = {
+    scales: {
+      r: {
+        suggestedMin: 0,  // Set your minimum value
+        suggestedMax: 5, // Set your maximum value
+        ticks: {
+          beginAtZero: true,
+          stepSize: 1,     // Set your desired step size
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false, // Set to false to hide the legend
+      },
+    },
+  };
+
   return (
     <Radar
-    updateMode='resize'
-    datasetIdKey='id'
-    
-    data={{
-      labels: data?.chartData?.labels,
-      datasets: [
-        {
-          label: data?.chartData?.datasets?.label,
-          data: data?.chartData?.datasets?.data,
-          backgroundColor: data?.animalDetail?.bgcolor,
-          borderColor: data?.animalDetail?.color,
-          borderWidth: 1,
-          
-        },
-      ],
-    }}
-    />
+      updateMode='resize'
+      datasetIdKey='id'
+      options={options}
+      data={{
 
+        labels: data?.chartData?.labels,
+        datasets: [
+          {
+            label: data?.chartData?.datasets?.label,
+            data: data?.chartData?.datasets?.data,
+            backgroundColor: data?.animalDetail?.bgcolor,
+            borderColor: data?.animalDetail?.color,
+            borderWidth: 1,
+          },
+        ],
+      }}
+    />
   )
 }
